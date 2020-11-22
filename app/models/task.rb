372614c2c06f5dbb111cd26576bb464b0fd83dd5
BrunaBelo <anabruna28@gmail.com>
2020-11-21 21:54:28 -0300

    adicionado validação dos campos

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
+  validates :description, :presence => true
+  validates :date, :presence => true
 
   scope :with_tag, proc { | tag |
     if tag.present?
