3a539896db0a71e4223c1b6ee91d810ddbfe84cf
BrunaBelo <anabruna28@gmail.com>
2020-11-22 14:47:55 -0300

    adicionado testes de modelo para tags

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @tag = Tag.new(name: 'Tag', color: '#F0FFFF')
+  end
+
+  test 'valid tag' do
+    assert @tag.save
+  end
+
+  test 'invalid without name' do
+    @tag.name = nil
+    assert_not @tag.save, "Saved the tag without a name"
+  end
+
+  test 'invalid without color' do
+    @tag.color = nil
+    assert_not @tag.save, "Saved the tag without a color"
+  end
 end
