41d9d55fab8180e59d11b147ec1269a7a35c1dee
BrunaBelo <anabruna28@gmail.com>
2020-11-22 14:51:41 -0300

    adicionado testes de modelo para tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(:email => 'user@gmail.com', :password => '123456', :password_confirmation => '123456')
+    @tags = Tag.create([{ name: 'Editorial', color: '#4d94ff' },{ name: 'Q1 Goals', color: '#d147a3' }])
+    @task = Task.create(:user => @user, :description => 'Quarterly newsletter', :date => '2020-11-20', :tags =>  [@tags[0]])
+  end
+
+  test 'valid taks' do
+    assert @task.save
+  end
+
+  test 'valid without taks' do
+    @task.tags = []
+    assert @task.save
+  end
+
+  test 'valid many taks' do
+    @task.tags = [@tags[0], @tags[1]]
+    assert @task.save
+  end
+
+  test 'invalid without description' do
+    @task.description = nil
+    assert_not @task.save
+  end
+
+  test 'invalid without date' do
+    @task.date = nil
+    assert_not @task.save
+  end
+
 end
